4fb07be99a9fc53c9197a526d8adfc935d73b790
zhukovln09-dotcom <zhukovln09@gmail.com>
2026-01-22 14:17:58 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Получите эти ключи из настроек вашего проекта Supabase
-const SUPABASE_URL = 'https://arcvzwxzohvbbwlmviit.supabase.co'; // Замените на ваш URL
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyY3Z6d3h6b2h2YmJ3bG12aWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwNzI1MDMsImV4cCI6MjA4NDY0ODUwM30.01LWHc3VZmCkLZdS07iPw-Q3elf89jOYiphkW1A0zvI'; // Замените на ваш public key
-
-// Инициализация Supabase клиента
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
